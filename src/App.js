932d064ac6cad0ba3414ccf128537404dfb40ca5
john1199 <jhon091199@gmail.com>
2021-09-05 14:58:51 -0500

    the Projects Provider is created

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Login from "./components/auth/Login";
 import Registro from "./components/auth/Registro";
 import Proyectos from "./components/proyectos/Proyectos";
 
+import ProyectoState from "./context/proyectos/ProyectoState";
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/registro" component={Registro} />
-        <Route exact path="/proyectos" component={Proyectos} />
-      </Switch>
-    </Router>
+    <ProyectoState>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/registro" component={Registro} />
+          <Route exact path="/proyectos" component={Proyectos} />
+        </Switch>
+      </Router>
+    </ProyectoState>
   );
 }
 
