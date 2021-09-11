5bf76dcc89842ebfccc17f1fbb918f643b07b2e5
john1199 <jhon091199@gmail.com>
2021-09-11 12:40:33 -0500

    se obtienen las tareas por proyecto

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,29 +1,30 @@
 import React, { useContext } from "react";
 import Tarea from "./Tarea";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import tareaContext from "../../context/tareas/tareaContext";
 
 const ListadoTareas = () => {
   const proyectosContext = useContext(proyectoContext);
   const { proyecto, eliminarProyecto } = proyectosContext;
+
+  const tareasContext = useContext(tareaContext);
+  const { tareasProyecto } = tareasContext;
+
   if (!proyecto) return <h2>Selecciona un nuevo proyecto</h2>;
+
   //Array destructuring
   const [proyectoActual] = proyecto;
-  const tareas = [
-    { nombre: "Plataforma", estado: true },
-    { nombre: "Colores", estado: false },
-    { nombre: "Pago", estado: true },
-    { nombre: "Hosting", estado: false },
-  ];
+  
   return (
     <>
       <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
-        {tareas.length === 0 ? (
+        {tareasProyecto.length === 0 ? (
           <li className="tarea">
             <p>No hay tareas</p>
           </li>
         ) : (
-          tareas.map((tarea) => <Tarea tarea={tarea} />)
+          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
         )}
       </ul>
       <button
--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from "react";
 import tareaContext from "./tareaContext";
 import tareaReducer from "./tareaReducer";
+import { TAREAS_PROYECTO } from "../../types";
+
 const tareas = [
   { nombre: "Plataforma", estado: true, proyectoId: 4 },
   { nombre: "Colores", estado: false, proyectoId: 4 },
@@ -15,12 +17,22 @@ const tareas = [
 const TareaState = (props) => {
   const initialState = {
     tareas: tareas,
+    tareasProyecto: null,
   };
 
   const [state, dispatch] = useReducer(tareaReducer, initialState);
 
+  const obtenerTareas = (proyectoId) => {
+    dispatch({ type: TAREAS_PROYECTO, payload: proyectoId });
+  };
   return (
-    <tareaContext.Provider value={{ tareas: state.tareas }}>
+    <tareaContext.Provider
+      value={{
+        tareas: state.tareas,
+        tareasProyecto: state.tareasProyecto,
+        obtenerTareas,
+      }}
+    >
       {props.children}
     </tareaContext.Provider>
   );
