aaa101b006e7e6d4f5ffabe5a68ea517e1696548
john1199 <jhon091199@gmail.com>
2021-09-11 10:48:37 -0500

    se agragan los types y validaciones en context proyectos

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const NuevoProyecto = () => {
+  const proyectosContext = useContext(proyectoContext);
+  const {
+    formulario,
+    errorFormulario,
+    mostrarFormulario,
+    agregarProyecto,
+    mostrarError,
+  } = proyectosContext;
+
   const [proyecto, setProyecto] = useState({ nombre: "" });
   const { nombre } = proyecto;
   const onChange = (e) => {
@@ -12,28 +22,46 @@ const NuevoProyecto = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    if (nombre === "") {
+      mostrarError();
+      return;
+    }
+    agregarProyecto(proyecto);
+
+    setProyecto({
+      nombre: "",
+    });
   };
   return (
     <>
-      <button type="button" className="btn btn-block btn-primario">
+      <button
+        type="button"
+        className="btn btn-block btn-primario"
+        onClick={() => mostrarFormulario()}
+      >
         Nuevo Proyecto
       </button>
 
-      <form className="formulario-nuevo-proyecto" onSubmit={onSubmit}>
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Nombre Proyecto"
-          name="nombre"
-          value={nombre}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          className="btn btn-block btn-primario"
-          placeholder="Agregar Proyecto"
-        />
-      </form>
+      {formulario ? (
+        <form className="formulario-nuevo-proyecto" onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Nombre Proyecto"
+            name="nombre"
+            value={nombre}
+            onChange={onChange}
+          />
+          <input
+            type="submit"
+            className="btn btn-block btn-primario"
+            placeholder="Agregar Proyecto"
+          />
+        </form>
+      ) : null}
+      {errorFormulario ? (
+        <p className="mensaje error">El nombre del proyecto es obligatorio</p>
+      ) : null}
     </>
   );
 };
--- src/context/proyectos/ProyectoState.js
+++ src/context/proyectos/ProyectoState.js
@@ -1,19 +1,63 @@
 import React, { useReducer } from "react";
-
+import { v4 as uuid } from "uuid";
 import proyectoContext from "./proyectoContext";
 import proyectoReducer from "./proyectoReducer";
+import {
+  FORMULARIO_PROYECTO,
+  OBTENER_PROYECTOS,
+  AGREGAR_PROYECTO,
+  VALIDAR_FORMULARIO,
+} from "../../types";
 
 const ProyectoState = (props) => {
+  const proyectos = [
+    { id: 1, nombre: "Tiendas" },
+    { id: 2, nombre: "Internet" },
+    { id: 3, nombre: "Diseño" },
+  ];
+
   const initialState = {
+    proyectos: [],
     formulario: false,
+    errorFormulario: false,
   };
   //Dispatch para ejecutar las acciones
   const [state, dispatch] = useReducer(proyectoReducer, initialState);
 
   //Serie de funciones para el CRUD
+  const mostrarFormulario = () => {
+    dispatch({
+      type: FORMULARIO_PROYECTO,
+    });
+  };
 
+  const obtenerProyectos = () => {
+    dispatch({ type: OBTENER_PROYECTOS, payload: proyectos });
+  };
+
+  const agregarProyecto = (proyecto) => {
+    proyecto.id = uuid();
+    console.log(proyecto.id);
+    dispatch({ type: AGREGAR_PROYECTO, payload: proyecto });
+  };
+
+  const mostrarError = () => {
+    dispatch({
+      type: VALIDAR_FORMULARIO,
+    });
+  };
   return (
-    <proyectoContext.Provider value={{ formulario: state.formulario }}>
+    <proyectoContext.Provider
+      value={{
+        proyectos: state.proyectos,
+        formulario: state.formulario,
+        errorFormulario: state.errorFormulario,
+        mostrarFormulario,
+        obtenerProyectos,
+        agregarProyecto,
+        mostrarError,
+      }}
+    >
       {props.children}
     </proyectoContext.Provider>
   );
